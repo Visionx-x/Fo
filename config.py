a15f5528f73474ab6f1b8df0e43bee1a42b0aec9
Visionx-x <telegrambott19@gmail.com>
2024-10-09 16:09:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,18 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-
+    API_ID = 21538384
+    API_HASH = "9b8e9b10a5c34b67054aceca02bf423e"
+    BOT_TOKEN = "8059875822:AAFPq_2ds5frfmcVo8M5fI72lueixdpCvLo"
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6757342838').split()]
+    BOT_SESSION = 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
+
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Avii12:Avii12@avii.rvyxw.mongodb.net/?retryWrites=true&w=majority&appName=Avii")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Avii12")
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 'https://t.me/+RFvG28BfB1tkMWVl'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/+aJBo4j5v3D1jMjA1") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
